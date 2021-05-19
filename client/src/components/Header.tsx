6847356d8c467e4b20489a719b37c27b6f28f7c5
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-05-19 16:08:24 +0200

    work on login/signup
    Co-authored-by: Lisa Pääjärvi <lisapaajarvi@users.noreply.github.com>

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties } from 'react';
+import React, { useState, CSSProperties } from 'react';
 import Badge from '@material-ui/core/Badge';
 import { withStyles } from '@material-ui/core/styles';
 import IconButton from '@material-ui/core/IconButton';
@@ -7,6 +7,14 @@ import { Link } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import { CartContext, CartProduct } from './contexts/CartContext';
 import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
+import ProfileCard from './ProfileCard'
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
 
 const StyledBadge = withStyles((theme) => ({
     badge: {
@@ -34,7 +42,76 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+
 function Header() { 
+    const [openLogin, setOpenLogin] = useState(false);
+    const [openSignup, setOpenSignup] = useState(false);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleLoginClose = () => {
+        setOpenLogin(false);
+    };
+    
+    function openLoginModal() {
+        setOpenLogin(true);
+    }
+    
+    const handleSignupClose = () => {
+        setOpenSignup(false);
+    };
+    
+    function openSignupModal() {
+        setOpenSignup(true);
+    }
+    
+    // const signup = ()=> {
+    //   const newUser = {
+    //     username: username,
+    //     password: password,
+    //   }
+    //   console.log(newUser)
+    //   axios
+    //     .post('/api/users/register', newUser)
+    //     .then(res => {
+    //       console.log(res)
+    //       setPassword('')
+    //       setUsername('')
+    //       setOpenSignup(false);
+    //       alert('New user created!');
+    //   })
+    // }
+    
+    // const login = ()=> {
+    //   const body = {
+    //     username: username,
+    //     password: password
+    //   }
+    //   axios
+    //     .post('/api/users/login', body)
+    //     .then(({ data: user }) => {
+    //       setUser(user);
+    //       setOpenLogin(false);          
+    //       setPassword('')
+    //       setUsername('')
+    //   })
+    // }
+    
+    const handleSignupUsername = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+      setUsername(e.target.value)
+    }
+    const handleSignupPassword = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+      setPassword(e.target.value)
+    }
+    
+    const handleLoginUsername = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+      setUsername(e.target.value)
+    }
+    
+    const handleLoginPassword = (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+      setPassword(e.target.value)
+    }
+
     const classes = useStyles();
     return (
         <CartContext.Consumer>
@@ -58,7 +135,88 @@ function Header() {
                                 </Link>
                             </Typography>
                         </div>
-                        <div style={{ marginRight: '1rem' }}>
+                        <div style={{ display: 'flex', marginRight: '1rem', alignItems: 'center' }}>
+
+
+                        {/* {!user? ( */}
+
+          <div className="buttonContainer">
+
+            <Button size="medium" variant="contained" onClick={openLoginModal}>LOGIN</Button>
+            <Button size="medium" variant="contained" onClick={openSignupModal}>SIGNUP</Button>
+
+          <Dialog open={openLogin} onClose={handleLoginClose} aria-labelledby="form-dialog-login">
+            <DialogTitle id="login">Login</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Username"
+                type="text"
+                onChange={handleLoginUsername}
+                defaultValue={username}
+                fullWidth
+                />
+              <TextField
+                margin="dense"
+                id="password"
+                label="Password"
+                type="password"
+                onChange={handleLoginPassword}
+                defaultValue={password}
+                fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleLoginClose} color="primary">
+                Go back
+                  </Button>
+              <Button onClick={handleLoginClose} variant="contained" color="primary">
+                Submit
+                  </Button>
+            </DialogActions>
+          </Dialog>
+
+          <Dialog open={openSignup} onClose={handleSignupClose} aria-labelledby="form-dialog-signup">
+            <DialogTitle id="signup">Signup</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Username"
+                type="text"
+                value={username}
+                onChange={handleSignupUsername}
+                fullWidth
+                />
+              <TextField
+                margin="dense"
+                id="password"
+                label="Password"
+                type="password"
+                onChange={handleSignupPassword}
+                value={password}
+                fullWidth
+                />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleSignupClose} color="primary">
+                Go back
+                  </Button>
+              <Button onClick={handleSignupClose} variant="contained" color="primary">
+                Register
+                  </Button>
+            </DialogActions>
+          </Dialog>
+
+        </div> 
+        {/* ):(  */}
+          <ProfileCard />
+          {/* )} */}
+      </div>
+      <div>
                             <Link style={linkStyle} to="/kundvagn">
                                 <IconButton aria-label="cart">
                                     <StyledBadge badgeContent={cartLength} color="secondary">
