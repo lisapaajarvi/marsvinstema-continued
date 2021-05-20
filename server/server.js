ce7b6af8f51c873a8d060abe35637a497cd355da
Camilla <73700387+millifrill@users.noreply.github.com>
2021-05-20 16:13:36 +0200

    added bcrypt .env

--- server/server.js
+++ server/server.js
@@ -1,23 +1,28 @@
-const  express = require('express');
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+const dotenv = require("dotenv");
 
-
-
+dotenv.config();
 
 // connects to the DB
 
-mongoose.connect(`mongodb+srv://Admin:marsvin123@cluster0.eexfj.mongodb.net/MarsvinsTema?retryWrites=true&w=majority`, {
-    useNewUrlParser : true,
-    useUnifiedTopology: true
- });
-console.log('You are now connected to your database!');
+mongoose
+	.connect(
+		`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eexfj.mongodb.net/MarsvinsTema?retryWrites=true&w=majority`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		},
+	)
+	.then(() => {
+		console.log("You are now connected to your database!");
+	})
+	.catch((error) => {
+		console.error(error);
+	});
+console.log("You are now connected to your database!");
 
-
-
-
-
-
-// listening to the server 
-app.listen(4000); 
+// listening to the server
+app.listen(5000);
