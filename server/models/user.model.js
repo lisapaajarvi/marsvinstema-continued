07f30a67db4994d39b2d0a6fbafa109f21e2d849
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-05-21 13:41:01 +0200

    userSchema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    // username: { type: String, required: true },
-    // password: { type: String, required: true },
-    // access: String
+     username: { type: String, required: true },
+     email: { type: String, required: true },
+     password: { type: String, required: true },
+     access: String
 });
 
 const UserModel = mongoose.model('user', userSchema);
