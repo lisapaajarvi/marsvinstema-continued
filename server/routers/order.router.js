55bdc8a540dd9052665ca9eacaca7f154e38dca2
Camilla <73700387+millifrill@users.noreply.github.com>
2021-05-24 11:49:48 +0200

    corrected misspelling

--- server/routers/order.router.js
+++ server/routers/order.router.js
@@ -3,11 +3,11 @@ const productRouter = express.Router();
 const controller = require("../controllers/product.controller");
 
 productRouter
-	.get("/product", controller.getPruduct)
+	.get("/product", controller.getProduct)
 	.get("/product/:id", controller.getProducts)
 	.get("/products/filtered", controller.filteredProducts)
 	.put("/product/:id", controller.editProduct)
 	.post("/products", controller.addNewProduct)
-	.deletet("/product/:id", controller.deleteProduct);
+	.delete("/product/:id", controller.deleteProduct);
 
 module.exports = productRouter;
