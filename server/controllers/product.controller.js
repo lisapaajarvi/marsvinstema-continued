afcf1a6885bf8e5a5bbdf31e993b6dcacc67bc3b
TonyMartinsson <tony.martinsson@hotmail.com>
2021-05-25 10:53:05 +0200

    Added getAllproduct and allProducts
    Co-authored-by: mrgling <mrgling@users.noreply.github.com>

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,12 +1,10 @@
-const bcrypt = require('bcrypt');
 const ProductModel = require("../models/product.model");
 
-exports.getProduct = (req, res) => {
+exports.getProduct = async (req, res) => {
 const { _id } = req.body;
 const product = await ProductModel.findOne({_id:_id});
     res.status(200).json(product); 
 }
-
 
 exports.getAllProducts = async (req, res) => {
     //lägg in middleware för admin check
@@ -15,3 +13,15 @@ exports.getAllProducts = async (req, res) => {
     console.log(products)
     res.status(200).json(products);
 }
+// exports.filteredProducts = async (req, res) => {
+ 
+// }
+// exports.editProduct = async (req, res) => {
+ 
+// }
+// exports.addNewProduct = async (req, res) => {
+ 
+// }
+// exports.deleteProduct = async (req, res) => {
+ 
+// }
--- server/routers/product.router.js
+++ server/routers/product.router.js
@@ -3,11 +3,11 @@ const productRouter = express.Router();
 const controller = require("../controllers/product.controller");
 
 productRouter
-	.get("/product/:id", controller.getProduct)
 	.get("/products", controller.getAllProducts)
-	.get("/products/filtered", controller.filteredProducts)
-	.put("/product/:id", controller.editProduct)
-	.post("/products", controller.addNewProduct)
-	.delete("/product/:id", controller.deleteProduct);
+	.get("/product/:id", controller.getProduct)
+	// .get("/products/filtered", controller.filteredProducts)
+	// .put("/product/:id", controller.editProduct)
+	// .post("/products", controller.addNewProduct)
+	// .delete("/product/:id", controller.deleteProduct);
 
 module.exports = productRouter;
