9bbcb9623ca20a67911cb6791caef68ebeb43a9b
LejlaDolovac <lejla.dolovac@squeed.com>
2021-05-26 15:02:08 +0200

    .

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema({
     price: {type: Number, required:  true},
     category: {type: String, required: true},
     title: {type: String, required: true},
-    // quantity?: Number,
+    // quantity: Number,
     // stock?: Number,
 })
 
