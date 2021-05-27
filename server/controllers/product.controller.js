834ee820e96588bd823cf5e57164792220465b1d
LejlaDolovac <lejla.dolovac@squeed.com>
2021-05-27 11:22:42 +0200

    Added logic for getProductsByCategory

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,5 +1,8 @@
 const ProductModel = require("../models/product.model");
 
+
+
+
 exports.getProduct = async (req, res) => {
     const { _id } = req.body;
     const product = await ProductModel.findOne({ _id: _id });
@@ -12,29 +15,38 @@ exports.getAllProducts = async (req, res) => {
     res.status(200).json(products);
 }
 
+
+exports.getAllCategories = async (req, res) => {
+    const products = await ProductModel.find({});
+    
+    let categories = [];
+    products.forEach(p => categories.push(...p.categories));
+    
+    // remove duplicates
+    categories = [...new Set(categories)];
+    
+    console.log(categories)
+    res.status(200).json(categories);
+}
+
+
+exports.getProductsByCategory = async (req, res) => {
+    const products = await ProductModel.find({})
+    categories = products.map(product => {
+        return {'title': product.title, 'categories': product.categories};
+    });
+    res.status(200).json(categories);
+    
+}
+
+
+
+
+
 // exports.addProduct = async (req, res) => {
 //     const addedProduct = await (await ProductModel.create(req.body));
 //     res.status(201).json(addedProduct);
 // }
-
-exports.getAllCategories = async (req, res) => {
-    const products = await ProductModel.find({});
-
-    let categories = [];
-    products.forEach(p => categories.push(...p.categories));
-
-    // remove duplicates
-    categories = [...new Set(categories)];
-
-    console.log(categories)
-    res.status(200).json(categories);
-}
-exports.getProductsByCategory = async (req, res) => {
-    const products = await ProductModel.find({})
-    console.log(products);
-    res.status(200).json(products);
-}
-
 
 // exports.editProduct = async (req, res) => {
 
