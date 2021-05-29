5ff19bd45827a8dfd9f8c5fd9b52d098a09a1f96
TonyMartinsson <tony.martinsson@hotmail.com>
2021-05-29 12:29:38 +0200

    Added static file upload

--- server/server.js
+++ server/server.js
@@ -7,8 +7,12 @@ const userRouter = require("./routers/user.router");
 const productRouter = require("./routers/product.router");
 const shippingMethodRouter = require("./routers/shippingmethod.router");
 const cookieSession = require('cookie-session');
+const fileUpload = require('express-fileupload');
 
 dotenv.config()
+
+// Static path for uploaded images
+app.use('/uploads', express.static('uploads'));
 
 // connects to the DB
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eexfj.mongodb.net/MarsvinsTema?retryWrites=true&w=majority`, {
@@ -33,6 +37,19 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS
     app.use("/api", userRouter);
     app.use("/api", productRouter);
     app.use("/api", shippingMethodRouter);
+    
+    // File Upload
+    app.post('/upload', fileUpload({ createParentPath: true }), (req, res) => {
+        console.log(req.files);
+        if(req.files?.image) {
+            const fileName = Date.now() + '-' + req.files.image.name
+            req.files.image.mv('uploads/' + fileName, () => {
+                res.status(200).send(fileName);
+            })
+        } else {
+            res.status(500).send();
+        }
+    });
     app.listen(4000); 
 })
 .catch((error)  => {
