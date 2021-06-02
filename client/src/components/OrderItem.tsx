f59bab056eb119b577aded6d51311438fa6dbee4
Lisa Pääjärvi <65873813+lisapaajarvi@users.noreply.github.com>
2021-06-02 10:46:02 +0200

    added date formatting

--- client/src/components/OrderItem.tsx
+++ client/src/components/OrderItem.tsx
@@ -7,27 +7,34 @@ interface Props {
   }
   
 export default function OrderItem(props: Props) {
-const { createdAt, _id} = props.order;
+    const { createdAt, _id} = props.order;
 
-function getDay(date:Date) {
-const dd = String(date.getDate()).padStart(2, '0');
-const mm = String(date.getMonth() + 1).padStart(2, '0'); 
-const yyyy = date.getFullYear();
-let day = yyyy + '-' + mm + '-' + dd;
-return day;
-}
+    const getOrderDate = () => {
+        const orderDate = new Date(createdAt);
+        let month:number|string = (orderDate.getMonth() + 1);
+        if (month < 10) {
+            month = '0' + month;
+        }
+        const year = orderDate.getFullYear();
+        let date : number|string = orderDate.getDate();
+        if (date < 10) {
+            date = '0' + date;
+        }
+        const formattedDate = year + '-' + month + '-' + date;
+        return formattedDate;
+    }
 
-return (
-    <>
-        <Grid item xs={5} sm={3} md={2} lg={2}>
-            <Typography variant="body1">Skapad</Typography>
-        </Grid>
-        <Grid item xs={6} sm={4} md={4} lg={6}>
-            <Typography variant="body1">{_id}</Typography>
-        </Grid>
-        <Grid item xs={4} sm={3} md={1} lg={1}>
-            <Typography variant="body1">Skickad</Typography>
-        </Grid>
-    </>
-);
+    return (
+        <>
+            <Grid item xs={5} sm={3} md={2} lg={2}>
+                <Typography variant="body1">{getOrderDate()}</Typography>
+            </Grid>
+            <Grid item xs={6} sm={4} md={4} lg={6}>
+                <Typography variant="body1">{_id}</Typography>
+            </Grid>
+            <Grid item xs={4} sm={3} md={1} lg={1}>
+                <Typography variant="body1">Skickad</Typography>
+            </Grid>
+        </>
+    );
 }
