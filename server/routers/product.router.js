3c121772af0d672ca52c8d1b085d3f08dde7e5c6
Jonas Glingert <jonas.glingert@medieinstitutet.se>
2021-06-02 13:53:39 +0200

    renamed endpoints, cleaned up

--- server/routers/product.router.js
+++ server/routers/product.router.js
@@ -4,13 +4,11 @@ const controller = require("../controllers/product.controller");
 const { checkAdminAccess, checkUserLogin } = require("../helpers/auth");
 
 productRouter
-	// Working
 	.get("/products", controller.getAllProducts)
-	.get("/product/:id", controller.getProduct)
+	.get("/products/:id", controller.getProduct)
 	.get("/products/categories", controller.getAllCategories)
 	.get('/products/category', controller.getProductsByCategory)
-	.put("/product/:id", checkAdminAccess, checkUserLogin, controller.editProduct)
+	.put("/products/:id", checkAdminAccess, checkUserLogin, controller.editProduct)
 	.post("/products", checkAdminAccess, controller.addProduct)
-	.delete("/product/:id", checkAdminAccess, controller.deleteProduct)
-
+	.delete("/products/:id", checkAdminAccess, controller.deleteProduct)
 module.exports = productRouter;
